973dccf27363031ce17f3023feefaaffe97fecbb
MahiroWatanabe <konchikuwa2:gmail.com>
2023-02-05 12:52:00 +0900

    20230205

--- submissions/46.py
+++ submissions/46.py
@@ -2,22 +2,22 @@ N = int(input())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
 C = list(map(int,input().split()))
-PA = set()
-PB = set()
-PC = set()
+
+cnt_a = [0]*46
+cnt_b = [0]*46
+cnt_c = [0]*46
 
 for i in range(N):
-    PA.add(A[i]%46)
-for i in range(N):
-    PB.add(A[i]%46)
-for i in range(N):
-    PC.add(A[i]%46)
+    cnt_a[A[i]%46] += 1
+    cnt_b[B[i]%46] += 1
+    cnt_c[C[i]%46] += 1
+    
 cnt = 0
 
-for i in list(PA):
-    for j in list(PB):
-        for k in list(PC):
-            if i+j+k % 46 == 0:
-                cnt += 1
+for i in range(46):
+    for j in range(46):
+        for k in range(46):
+            if (i+j+k) % 46 == 0:
+                cnt += cnt_a[i] * cnt_b[j] * cnt_c[k]
 
 print(cnt)
